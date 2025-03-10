aaea0728c32dbf048a3af66deccfe80e54fe332a
alcehmystic <bene.falin@gmail.com>
2025-03-10 03:11:34 -0400

    Uploading Working Build

--- Assets/Scripts/SpriteManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class SpriteManager
-{
-    public static Sprite[] sprites = Resources.LoadAll<Sprite>("Sprites/SpriteSheet");
-}
