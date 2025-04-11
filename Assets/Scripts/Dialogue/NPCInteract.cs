c9305c84a3bc591f0ea6440624058ab7fd2fe0f4
William Rowley <150847662+Whiskk@users.noreply.github.com>
2025-04-11 14:03:51 -0400

    Made dialogue work with NPC

---
+++ Assets/Scripts/Dialogue/NPCInteract.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using TMPro;
+
+public class NPCInteract : MonoBehaviour
+{
+    private bool playerInRange = false;
+    private DialogueTrigger dialogueTrigger;
+
+    public GameObject talkPromptUI; // Reference the GameObject holding the TMP text
+
+    void Start()
+    {
+        dialogueTrigger = GetComponent<DialogueTrigger>();
+
+        if (talkPromptUI != null)
+            talkPromptUI.SetActive(false); // Hide at start
+    }
+
+    void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            dialogueTrigger.TriggerDialogue();
+
+            if (talkPromptUI != null)
+                talkPromptUI.SetActive(false); // Optionally hide on dialogue start
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+
+            if (talkPromptUI != null)
+                talkPromptUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+
+            if (talkPromptUI != null)
+                talkPromptUI.SetActive(false);
+        }
+    }
+}
