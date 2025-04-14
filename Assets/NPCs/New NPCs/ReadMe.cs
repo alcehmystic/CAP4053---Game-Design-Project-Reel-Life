ee1fd2870c47bd7124bbf093041c251a0321af08
Rhynhelt <hib8882@gmail.com>
2025-04-13 22:53:44 -0400

    Finished Playermodel & NPCs
    For whoever wants to manipulate NPC colors, below is the readme that's also in the folder for cloth color changing:
    
    /*
     * Hey guys, so to use the different colors of hats/shirts/pants for the NPCs,
     * click on the model and open the "Material" tab on the right side. Then,
     * click on the circle button on the right side of materials and search for a
     * color (or just type out the type of clothing) and choose the type you want
     * the NPC to have. If you make a different combination, make sure to copy and
     * paste that into a new model otherwise it'll overrite all the NPCs using that
     * same model. Below are the clothing options to pick from:
     *
     * SHIRTS:
     * Blue
     * Red (Search for "Playermodel")
     * Black
     * Green
     * White
     *
     * JEANS:
     * Black
     * Blue (Search for "Playermodel")
     *
     * HATS/SHOES:
     * Brown (Search for "Playermodel")
     * White
     * */

---
+++ Assets/NPCs/New NPCs/ReadMe.cs
@@ -0,0 +1,24 @@
+/*
+ * Hey guys, so to use the different colors of hats/shirts/pants for the NPCs,
+ * click on the model and open the "Material" tab on the right side. Then,
+ * click on the circle button on the right side of materials and search for a
+ * color (or just type out the type of clothing) and choose the type you want
+ * the NPC to have. If you make a different combination, make sure to copy and
+ * paste that into a new model otherwise it'll overrite all the NPCs using that
+ * same model. Below are the clothing options to pick from:
+ * 
+ * SHIRTS:
+ * Blue
+ * Red (Search for "Playermodel")
+ * Black
+ * Green
+ * White
+ * 
+ * JEANS:
+ * Black
+ * Blue (Search for "Playermodel")
+ * 
+ * HATS/SHOES:
+ * Brown (Search for "Playermodel")
+ * White
+ * */
